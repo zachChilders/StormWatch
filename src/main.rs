603627e67015043d9e599e3da648568d586ca4d2
zachChilders <zach@childers.dev>
2020-12-28 06:53:47 -0800

    dope

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use eyre::Result;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
 
 #[derive(Debug, Deserialize)]
 struct WeatherResponse {
@@ -25,8 +25,7 @@ struct Hourly {
     wind_deg: f64,
     rain: Option<Rain>,
     snow: Option<Snow>,
-    #[serde(skip_deserializing)]
-    weather: Option<String>,
+    weather: Vec<Weather>,
     pop: f64,
 }
 
@@ -40,35 +39,60 @@ struct Rain {
     #[serde(rename(deserialize = "1h"))]
     hour: f64,
 }
+#[derive(Debug, Deserialize)]
+struct Weather {
+    id: f64,
+    main: String,
+    description: String,
+    icon: String,
+}
 
-fn get_weather() -> Result<()> {
+type Coords = (f64, f64, &'static str);
+
+const MAMMOTH: Coords = (37.6482765, -118.9832411, "Mammoth");
+const RENO: Coords = (39.5197729,-119.9283731, "Reno");
+const SLT: Coords = (38.9227921,-120.013534, "South Lake");
+
+fn get_weather(coordinates: Coords) -> Result<()> {
     let key = "115310efdc47011d9ab0ca9a2ea1fb23";
 
-    let lat: f64 = 37.6482765;
-    let long: f64 = -118.9832411;
-
-    let uri = format!(
+    let api_uri = format!(
         "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&exclude=minutely,current,daily&appid={key}&units=imperial",
-        lat = lat,
-        long = long,
+        lat = coordinates.0,
+        long = coordinates.1,
         key = key
     );
 
-    let body = reqwest::blocking::get(&uri)?.text()?;
+    let body = reqwest::blocking::get(&api_uri)?.text()?;
     let response: WeatherResponse = serde_json::from_str(&body)?;
 
     let hourly = response.hourly;
 
+    const MM_IN_INCHES: f64 = 25.4;
+    let mut total_snowfall = 0.0;
     for (i, hour) in hourly.iter().enumerate() {
-        if hour.snow.is_some() {
-            println!("It's going to snow in {i} hours", i=i);
+        if let Some(snow) = &hour.snow {
+            println!(
+                "It's going to snow {mm}mm in {i} hours",
+                mm = snow.hour,
+                i = i
+            );
+            total_snowfall += snow.hour;
         }
     }
+    println!(
+        "Total snowfall for the next 48 hours in {}: {:?} inches",
+        coordinates.2,
+        total_snowfall / MM_IN_INCHES
+    );
 
     Ok(())
 }
 fn main() -> Result<(), reqwest::Error> {
-    get_weather().unwrap();
-
+    get_weather(MAMMOTH).unwrap();
+    println!("==========================");
+    get_weather(RENO).unwrap();
+    println!("==========================");
+    get_weather(SLT).unwrap();
     Ok(())
 }
